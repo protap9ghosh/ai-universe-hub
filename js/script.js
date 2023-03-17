343fd6195360ff890213a46659a48367d36b80d1
protap9ghosh <protapg518@gmail.com>
2023-03-17 18:47:20 +0600

    show six data

--- js/script.js
+++ js/script.js
@@ -9,3 +9,45 @@ const fetchData = () => {
     console.log(err);
   }
 };
+
+
+// This function determines how the data will appear on the display
+const displayDataFunc = (allToolsData) => {
+  const mainContainer = document.getElementById("main-container");
+  const showAllBtn = document.getElementById("showAll-btn");
+  const sortByDateBtn = document.getElementById("sortByDateBtn");
+
+  // The following function works on how the data will be shown on the display in an ascending manner through the date.
+  sortByDateBtn.addEventListener("click", () => {
+    sortByDateBtn.style.cursor = "not-allowed";
+    const compareDates = (dateOne, dateTwo) => {
+      const firstDate = new Date(
+        dateOne.published_in.split("/").reverse().join("-")
+      );
+      const secondDate = new Date(
+        dateTwo.published_in.split("/").reverse().join("-")
+      );
+
+      return firstDate - secondDate;
+    };
+    allToolsData.sort(compareDates);
+    displayDataFunc(allToolsData);
+  });
+
+
+  // First show 6 data and then how to show all data by making button visible through condition.
+  const firstSixItems = allToolsData.slice(0, 6);
+  if (allToolsData.length > firstSixItems.length) {
+    showAllBtn.style.display = "block";
+    showAllBtn.addEventListener("click", () => {
+      mainContainer.innerHTML = "";
+      displayShowData(allToolsData);
+
+      showAllBtn.style.display = "none";
+    });
+  } else {
+    showAllBtn.style.display = "none";
+  }
+
+  displayShowData(firstSixItems);
+};
